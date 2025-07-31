a954791835d26e6e14732399c837b72caa77a5fa
ErickIgles <erick.almeida2125@gmail.com>
2025-07-31 05:55:12 -0300

    feat: adiciona modelo Tarefa ao app tarefas

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Base(models.Model):
+    criado = models.DateField(verbose_name='Criardo', auto_now_add=True)
+    modificado = models.DateTimeField(verbose_name='Atualizado', auto_now=True)
+    ativo = models.BooleanField(verbose_name='Ativo', default=True)
+
+    class Meta:
+        abstract = True
+
+
+
+class Tarefa(Base):
+    STATUS_CHOICES = [
+        ('P', 'Pendente'),
+        ('A', 'Em andamento'),
+        ('C', 'Concluído')
+    ]
+
+    titulo = models.CharField(verbose_name='Título', max_length=70)
+    descricao = models.TextField(verbose_name='Descrição')
+    status = models.CharField(verbose_name='Status', max_length=1, choices=STATUS_CHOICES, default='P')
+    usuario = models.ForeignKey(User, verbose_name='Usuário', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'{self.titulo}'
+    
