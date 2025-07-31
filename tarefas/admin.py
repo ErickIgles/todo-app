43b6d8b1be917bd92ec06c7f4d2dfe8fd0644431
ErickIgles <erick.almeida2125@gmail.com>
2025-07-31 05:57:18 -0300

    feat: adiciona modelo Tarefa ao admin

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tarefa
+
+
+@admin.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'titulo', 'descricao', 'usuario', 'criado', 'modificado', 'ativo']
+    list_display_links = ['id', 'titulo', 'descricao', 'usuario']
+
