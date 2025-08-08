b5fdff959e674f7cf63292c6b136dc38bfe2e0db
ErickIgles <erick.almeida2125@gmail.com>
2025-08-07 21:26:28 -0300

    feat: adiciona modelo Task ao app

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+
+    criado = models.DateField(verbose_name='Criado', auto_now_add=True)
+    atualizado = models.DateTimeField(verbose_name='Atualizado', auto_now=True)
+    ativo = models.BooleanField(verbose_name='Ativo', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Task(Base):
+
+    class Status(models.TextChoices):
+        PENDENTE = 'pendente', 'Pendente'
+        EM_ANDAMENTO = 'em_andamento', 'Em andamento'
+        CONCLUIDA = 'concluida', 'Concluída'
+
+    titulo = models.CharField(
+        verbose_name='Título', 
+        max_length=100
+        )
+    descricao = models.TextField(
+        verbose_name='Descrição', 
+        blank=True, 
+        null=True
+        )
+    status = models.BooleanField(
+        verbose_name='Status',
+        choices=Status.choices,
+        default=Status.PENDENTE,
+        max_length=20
+        )
+    
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+    
+    def __str__(self):
+        return f'{self.titulo}'
+
