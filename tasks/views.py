b877538f0bd89f95f03dde63facf20f29e89a7e6
ErickIgles <erick.almeida2125@gmail.com>
2025-08-09 19:57:40 -0300

    feat: adiciona funcionalidade para o cadastro de task e validação de formulário

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
 
-from django.views.generic import TemplateView
-
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+from .forms import TaskModelForm
 
 
 
 class BaseTemplateView(TemplateView):
     template_name = 'base.html'
+
+
+
+class TarefaCriarView(CreateView):
+    template_name = 'tasks/form_add.py'
+    form_class = TaskModelForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        return super().form_invalid(form)
 
