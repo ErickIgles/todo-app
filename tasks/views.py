3522b996a06f5fc914f1a796ea815601e831bba5
ErickIgles <erick.almeida2125@gmail.com>
2025-08-09 19:36:04 -0300

    refactor: reestrutura view do template base para o formato de classe

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
 
-def home(request):
-    return render(request, 'base.html')
+
+class BaseTemplateView(TemplateView):
+    template_name = 'base.html'
+
