2abbcf801caa0834df436d259fc30f8c46d42760
ErickIgles <erick.almeida2125@gmail.com>
2025-08-09 20:09:35 -0300

    feat: adiciona rota para cadastro de task e define app_name

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'tasks'
 urlpatterns = [
     path('', views.BaseTemplateView.as_view(), name='home'),
+    path('tarefa/cadastro/', views.TarefaCriarView.as_view(), name='cadastro'),
 ]
