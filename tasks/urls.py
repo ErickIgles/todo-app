6216261a7d82970473f26b31a068e18aac90562e
ErickIgles <erick.almeida2125@gmail.com>
2025-08-09 21:49:39 -0300

    feat: adiciona rota url para a view de listagem de task

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ app_name = 'tasks'
 urlpatterns = [
     path('', views.BaseTemplateView.as_view(), name='home'),
     path('tarefa/cadastro/', views.TarefaCriarView.as_view(), name='cadastro'),
+    path('tarefa/lista/', views.TarefaListaView.as_view(), name='listar'),
 ]
