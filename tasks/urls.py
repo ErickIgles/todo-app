9174d113a2b477a27ee1f1ae5e4fef0bbceb416b
ErickIgles <erick.almeida2125@gmail.com>
2025-08-09 22:06:33 -0300

    feat: adiciona url para a view de atualização de task

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.BaseTemplateView.as_view(), name='home'),
     path('tarefa/cadastro/', views.TarefaCriarView.as_view(), name='cadastro'),
     path('tarefa/lista/', views.TarefaListaView.as_view(), name='listar'),
+    path('tarefa/atualizar/<int:pk>/', views.TarefaAtualizarView.as_view(), name='atualizar'),
 ]
