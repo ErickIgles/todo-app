24491bfaf23c06ee33af791d26057e5ef455d1bb
ErickIgles <erick.almeida2125@gmail.com>
2025-08-09 22:24:30 -0300

    refactor: altera nome TarefaListaView para TarefaListarView e padronização de url

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,7 +6,7 @@ app_name = 'tasks'
 urlpatterns = [
     path('', views.BaseTemplateView.as_view(), name='home'),
     path('tarefa/cadastro/', views.TarefaCriarView.as_view(), name='cadastro'),
-    path('tarefa/lista/', views.TarefaListaView.as_view(), name='listar'),
+    path('tarefa/listar/', views.TarefaListarView.as_view(), name='listar'),
     path('tarefa/atualizar/<int:pk>/', views.TarefaAtualizarView.as_view(), name='atualizar'),
     path('tarefa/deletar/<int:pk>/', views.TarefaDeletarView.as_view(), name='deletar'),
 ]
