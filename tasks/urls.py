b73ff811e886296ccd0e8d1926e06c6967bce913
ErickIgles <erick.almeida2125@gmail.com>
2025-08-09 22:27:45 -0300

    refactor: altera name de 'cadastro' para 'cadastrar' e padronização de url

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'tasks'
 urlpatterns = [
     path('', views.BaseTemplateView.as_view(), name='home'),
-    path('tarefa/cadastro/', views.TarefaCriarView.as_view(), name='cadastro'),
+    path('tarefa/cadastrar/', views.TarefaCriarView.as_view(), name='cadastrar'),
     path('tarefa/listar/', views.TarefaListarView.as_view(), name='listar'),
     path('tarefa/atualizar/<int:pk>/', views.TarefaAtualizarView.as_view(), name='atualizar'),
     path('tarefa/deletar/<int:pk>/', views.TarefaDeletarView.as_view(), name='deletar'),
