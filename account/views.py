01ce666658c69d74ef16854f8a320137d0125cbd
ErickIgles <erick.almeida2125@gmail.com>
2025-08-10 19:34:42 -0300

    feat: adiciona view para cadastro de usuário

--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import UserForm
+
+
+
+class UsuarioCriarView(CreateView):
+    template_name = 'account/form_register.html'
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('tasks:home')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
