bfec8b6277bf3bd373dcecdc0dce473b5456445a
ErickIgles <erick.almeida2125@gmail.com>
2025-08-10 08:49:42 -0300

    feat: adiciona  widgets personalizados ao formulário

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-
+from django import forms
 
 from .models import Task
 
@@ -10,4 +10,8 @@ class TaskModelForm(ModelForm):
     class Meta:
         model = Task
         fields = ['titulo', 'descricao', 'status']
-
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': ''}),
+            'descricao': forms.Textarea(attrs={'class': ''}),
+            'status': forms.Select(attrs={'class': ''})
+        }
