46a7b6cb52e77e98c9aaea48c87ec27a8096eeb9
ErickIgles <erick.almeida2125@gmail.com>
2025-09-07 12:19:08 -0300

    feat: adiciona rotas para registro, login e logout de usuário

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'account'
 urlpatterns = [
-    path('', views.UsuarioCriarView.as_view(), name='registrar'),
+    path(
+        'registrar/',
+        views.UsuarioCriarView.as_view(),
+        name='registrar'
+    ),
+    path(
+        'login/',
+        views.UsuarioLoginView.as_view(),
+        name='login'
+    ),
+    path(
+        'logout/',
+        views.UsuarioLogoutView.as_view(),
+        name='logout'
+    )
 ]
-
