083e7b16a8dcb9e5fd5b50aa5f3357a5b915ab86
ErickIgles <erick.almeida2125@gmail.com>
2025-09-07 17:47:50 -0300

    feat: inclui o usuário nos kwargs do formulário via get_form_kwargs

--- tasks/views.py
+++ tasks/views.py
@@ -1,34 +1,64 @@
 
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView
+from django.views.generic import (
+    TemplateView,
+    CreateView,
+    ListView,
+    UpdateView,
+    DeleteView
+)
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .forms import TaskModelForm
 
 from .models import Task
 
-class BaseTemplateView(TemplateView):
+
+class BaseTemplateView(
+    LoginRequiredMixin,
+    TemplateView
+):
     template_name = 'base.html'
 
 
-
-class TarefaCriarView(CreateView):
+class TarefaCriarView(
+    LoginRequiredMixin,
+    CreateView
+):
     template_name = 'tasks/form_add.html'
     form_class = TaskModelForm
     success_url = reverse_lazy('tasks:listar')
 
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+
+        usuario = self.request.user
+
+        kwargs['usuario'] = usuario
+
+        return kwargs
+
     def form_valid(self, form):
         return super().form_valid(form)
-    
+
     def form_invalid(self, form):
         return super().form_invalid(form)
 
 
-class TarefaListarView(ListView):
+class TarefaListarView(
+    LoginRequiredMixin,
+    ListView
+):
     template_name = 'tasks/list.html'
     queryset = Task.objects.all()
     context_object_name = 'tasks'
 
 
-class TarefaAtualizarView(UpdateView):
+class TarefaAtualizarView(
+    LoginRequiredMixin,
+    UpdateView
+):
     template_name = 'tasks/form_update.html'
     model = Task
     form_class = TaskModelForm
@@ -37,14 +67,16 @@ class TarefaAtualizarView(UpdateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
-    
+
     def form_invalid(self, form):
         return super().form_invalid(form)
 
 
-class TarefaDeletarView(DeleteView):
+class TarefaDeletarView(
+    LoginRequiredMixin,
+    DeleteView
+):
     template_name = 'tasks/form_delete.html'
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks:listar')
-
