ef1af494184b73f8b906e213256e3272d23be7b4
ErickIgles <erick.almeida2125@gmail.com>
2025-09-07 12:18:21 -0300

    feat: adiciona views de login e logout com UserLoginForm e mensagens de feedback no cadastro

--- account/views.py
+++ account/views.py
@@ -1,10 +1,13 @@
 
+from django.contrib import messages
 from django.contrib.auth.models import User
-
-from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from .forms import UserForm
+from django.views.generic import CreateView
 
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+from .forms import UserForm, UserLoginForm
 
 
 class UsuarioCriarView(CreateView):
@@ -14,8 +17,25 @@ class UsuarioCriarView(CreateView):
     success_url = reverse_lazy('tasks:home')
 
     def form_valid(self, form):
+        messages.success(
+            self.request,
+            'Usuário cadastrado com sucesso.'
+        )
         return super().form_valid(form)
-    
+
     def form_invalid(self, form):
+        messages.error(
+            self.request,
+            'Erro ao realizar o cadastro. Verifique às informações.'
+        )
         return super().form_invalid(form)
 
+
+class UsuarioLoginView(LoginView):
+    template_name = 'account/form_login.html'
+    redirect_authenticated_user = True
+    form_class = UserLoginForm
+
+
+class UsuarioLogoutView(LogoutView):
+    next_page = 'account:login'
