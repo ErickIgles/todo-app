2821eea12e6593767eab54b01b1fbbb8df57d293
ErickIgles <erick.almeida2125@gmail.com>
2025-09-07 17:52:07 -0300

    feat: recupera usuário pelo  __init__ e adiciona  método save  para associar usuário à tarefa

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,7 +4,6 @@ from django import forms
 from .models import Task
 
 
-
 class TaskModelForm(ModelForm):
 
     class Meta:
@@ -15,3 +14,21 @@ class TaskModelForm(ModelForm):
             'descricao': forms.Textarea(attrs={'class': 'form-control'}),
             'status': forms.Select(attrs={'class': 'form-control'})
         }
+
+    def __init__(self, *args, **kwargs):
+        self.usuario = kwargs.pop('usuario', None)
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+
+        tarefa = super().save(commit=False)
+
+        if self.usuario is not None:
+
+            tarefa.usuario = self.usuario
+
+        if commit:
+
+            tarefa.save()
+
+        return tarefa
